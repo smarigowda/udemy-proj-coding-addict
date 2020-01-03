ad7e4a898938636d497e8239f0ec3ccdefe710e0
Santosh Marigowda <santosharakere@gmail.com>
2020-01-03 05:27:00 +0000

    skills html

--- from-scratch/js/script.js
+++ from-scratch/js/script.js
@@ -1,7 +1,9 @@
 $(document).ready(() => {
+    $('#links').toggle(100);
     $('#menu-btn').click(changeNav);
 
     function changeNav() {
+        $('#links').toggle(100);
         $('#menu-btn').toggleClass("turn");
     }
 })
